cce95cf70880f74544691c5af0dad17691b5218a
Shruti Zarbade <shrutizarbade22@gmail.com>
2020-02-24 12:42:11 +0530

    added: the function of reset board and display board

---
+++ tic_tac_toe.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+echo "Welcome to Tic Tac Toe Game"
+
+declare -A board
+total_cell=9
+function resetboard()
+{
+   for (( i=1; i<=$total_cell; i++ ))
+   do
+      board[$i]=""
+   done
+}
+
+function display_board()
+{
+   echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
+   echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
+   echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
+}
+resetboard
+display_board
+
